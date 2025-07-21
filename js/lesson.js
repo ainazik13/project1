f594304e91f46c15e7c798f69d253919ae26b4f7
ainazik13 <mirlankyzyainazik13@gmail.com>
2025-07-21 16:16:55 +0600

    project

--- js/lesson.js
+++ js/lesson.js
@@ -62,4 +62,61 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 
+//Converter
 
+const somInput = document.querySelector('#som')
+const usdInput = document.querySelector('#usd')
+const eurInput = document.querySelector('#eur')
+const krwInput = document.querySelector('#krw')
+
+const converter = (element, currency) => {
+  element.oninput = () => {
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', '../data/converter.json')
+    xhr.setRequestHeader('Content-type', 'application/json')
+    xhr.send()
+
+    xhr.onload = () => {
+      const data = JSON.parse(xhr.response)
+
+      const rates = {
+        usd: data.usd,
+        eur: data.eur,
+        krw: data.krw
+      }
+
+      const value = parseFloat(element.value)
+      if (isNaN(value)) {
+        somInput.value = ''
+        usdInput.value = ''
+        eurInput.value = ''
+        krwInput.value = ''
+        return
+      }
+
+
+      let somValue
+      if (currency === 'som') somValue = value
+      else somValue = value * rates[currency]
+
+
+      if (currency !== 'som') somInput.value = somValue.toFixed(2)
+      if (currency !== 'usd') usdInput.value = (somValue / rates.usd).toFixed(2)
+      if (currency !== 'eur') eurInput.value = (somValue / rates.eur).toFixed(2)
+      if (currency !== 'krw') krwInput.value = (somValue / rates.krw).toFixed(2)
+
+
+      if (element.value === '') {
+        somInput.value = ''
+        usdInput.value = ''
+        eurInput.value = ''
+        krwInput.value = ''
+      }
+    }
+  }
+}
+
+converter(somInput, 'som')
+converter(usdInput, 'usd')
+converter(eurInput, 'eur')
+converter(krwInput, 'krw')
